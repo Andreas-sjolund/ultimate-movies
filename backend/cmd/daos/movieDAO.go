326e8d1b8942c49b1a38676425026bcb51d94a7c
Johan Eriksson <johan.eriksson@chas.se>
2019-10-30 11:01:23 +0100

    added movieDAO

--- backend/cmd/daos/movieDAO.go
+++ backend/cmd/daos/movieDAO.go
@@ -9,8 +9,8 @@ import (
 	"gopkg.in/mgo.v2/bson"
 )
 
-// MoviesDAO defines data access object
-type MoviesDAO struct {
+// MovieDAO defines data access object
+type MovieDAO struct {
 	Server   string
 	Database string
 }
@@ -23,7 +23,7 @@ const (
 )
 
 // Connect establishes a connection to the database
-func (m *MoviesDAO) Connect() {
+func (m *MovieDAO) Connect() {
 	session, err := mgo.Dial(m.Server)
 	if err != nil {
 		log.Fatal(err)
@@ -32,7 +32,7 @@ func (m *MoviesDAO) Connect() {
 }
 
 // FindAll movies
-func (m *MoviesDAO) FindAll() ([]models.Movie, error) {
+func (m *MovieDAO) FindAll() ([]models.Movie, error) {
 	var movies []models.Movie
 	err := db.C(COLLECTION).Find(bson.M{}).All(&movies)
 	if err != nil {
@@ -42,7 +42,7 @@ func (m *MoviesDAO) FindAll() ([]models.Movie, error) {
 }
 
 //FindByID finds a movie based on ID
-func (m *MoviesDAO) FindByID(id string) (models.Movie, error) {
+func (m *MovieDAO) FindByID(id string) (models.Movie, error) {
 	var movie models.Movie
 	err := db.C(COLLECTION).FindId(bson.ObjectIdHex(id)).One(&movie)
 	if err != nil {
@@ -52,7 +52,7 @@ func (m *MoviesDAO) FindByID(id string) (models.Movie, error) {
 }
 
 // Insert a movie into DB
-func (m *MoviesDAO) Insert(movie models.Movie) error {
+func (m *MovieDAO) Insert(movie models.Movie) error {
 	err := db.C(COLLECTION).Insert(&movie)
 	if err != nil {
 		panic(err)
@@ -61,7 +61,7 @@ func (m *MoviesDAO) Insert(movie models.Movie) error {
 }
 
 // Delete an exsisting movie
-func (m *MoviesDAO) Delete(movie models.Movie) error {
+func (m *MovieDAO) Delete(movie models.Movie) error {
 	err := db.C(COLLECTION).Remove(&movie)
 	if err != nil {
 		panic(err)
@@ -70,7 +70,7 @@ func (m *MoviesDAO) Delete(movie models.Movie) error {
 }
 
 // Update an existing movie
-func (m *MoviesDAO) Update(movie models.Movie) error {
+func (m *MovieDAO) Update(movie models.Movie) error {
 	err := db.C(COLLECTION).UpdateId(movie.ID, &movie)
 	if err != nil {
 		panic(err)
